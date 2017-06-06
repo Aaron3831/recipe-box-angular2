10126d3f0d705ff67ef3d8c2b76fb28efbceb9bc
Aaron Nguyen <aaron3831@gmail.com>
2017-06-06 14:29:50 -0700

    basic hard-coded recipe with title, ingredients, directions and rating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Recipe Box';
+  recipes: Recipe[] = [
+    new Recipe('Bell Peppers and Beef', 'Beef, Bell Peppers, olive oil, onions, garlic, mushrooms, soy sauce, teriyaki sauce', 'Peel the garlic and cut off the cap. Pour the olive oil into a pan/wok and stir-fry onion, bell pepper, mushrooms and garlic with the butter for about 5 minutes or less. Add ground beef and fry till cooked to taste. Take bouillon cube and crumble over the mixture, along with the soy and teriyaki sauce.', 4)
+  ];
+}
+
+export class Recipe {
+  constructor(public title: string, public ingredients: string, public instructions: string, public rating: number){ }
 }
